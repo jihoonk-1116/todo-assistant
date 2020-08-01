6d9c227763405054044c3f7610431c71211ae0f1
Chihoon Kim <mynoja3@gmail.com>
2020-08-01 14:37:51 -0400

    Implementation add, delete method. Index+1 in the method of show. Change main to use while loop and to input command

--- functions/delete.py
+++ functions/delete.py
@@ -1,8 +1,18 @@
 def delete(todos):
-    print("Enter number what you want to remove")
-    enter = int(input()) - 1
-    for i, k in enumerate(todos):
-        if i == enter:
-            del todos[k]
-            break
+    print("Enter the priority number what you want to remove")
+    enter = str(input())
+    print(todos)
+    index = int(enter[1])-1
+    if enter[0] == 'h':
+        for i,v in enumerate(todos['high']):
+            if index == i:
+                del todos['high'][i]
+    if enter[0] == 'm':
+        for i,v in enumerate(todos['medium']):
+            if index == i:
+                del todos['medium'][i]
+    if enter[0] == 'l':
+        for i,v in enumerate(todos['low']):
+            if index == i:
+                del todos['low'][i]
     return todos
--- todo_ssistant.py
+++ todo_ssistant.py
@@ -6,21 +6,19 @@ from functions.parse_todos import parse
 
 todos = parse("todo_list.json")
 
-show(todos)
 
-# while True:
-#     print("Do you want me to check off any from the list? Enter the corresponding number to check off.")
-#     print("If you want to exit this program, enter x")
-#     show(todos)
-#     entered = str(input())
-#     if entered == '0':
-#         todos = add(todos)
-#     if entered == 'x':
-#         break
-#     if entered == 'd':
-#         todos = delete(todos)
+while True:
+     print("Do you want me to check off any from the list? Enter the corresponding number to check off.")
+     print("If you want to exit this program, enter x")
+     show(todos)
+     entered = str(input())
+     if entered == '0':
+         todos = add(todos)
+     if entered == 'x':
+         break
+     if entered == 'd':
+         todos = delete(todos)
 
-# print("Bye")
+print("Bye")
 
-# show(todos)
 

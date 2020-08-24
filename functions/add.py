ea2f4bcae7393acf370f1923e7680cc6db844871
jihoon91 <jihoon91@users.noreply.github.com>
2020-08-23 22:56:47 -0400

    Implemented add_module, remove_module

--- functions/add.py
+++ functions/add.py
@@ -1,13 +1,11 @@
-def add(todos):
-    print("Enter To do list and priority(h,m,l)")
-    enter = str(input())
-    prior = str(input())
-    #todos[pri][list][key]
-    if prior == 'h':
-        todos['high'].append((enter,False))
-    if prior == 'm':
-        todos['medium'].append((enter,False))
-    if prior == 'l':
-        todos['low'].append((enter,False))
+from model.todo import Todo
 
-    return todos
+def add(todos, context, priority):
+    priority_map = {'h':'high', 'm':'medium', 'l':'medium'}
+    priority = priority_map.get(priority)
+    todo = Todo(context, False, priority)
+    todos[priority].append(todo)
+    
+    print("{} has been added.".format(context))
+
+    return
